1f3522db85f1bbdd8a9812cf39c341911c067e12
jetsontwo <jetsontwo@gmail.com>
2016-10-13 22:02:30 -0700

    Created Basic Character Movement
    Allowed character to move left and right.

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+    public float speed;
+    private int horiz_move;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            horiz_move = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            horiz_move = -1;
+        }
+        else
+        {
+            horiz_move = 0;
+        }
+
+        rb.AddForce(new Vector2(horiz_move * speed, 0));
+        
+    }
+}
