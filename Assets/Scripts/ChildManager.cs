9dec4036f086eecfcd0ecbfe4c20b8612f6f7f9f
Roger <roger@dalkefamily.com>
2016-10-19 01:05:19 -0700

    Got child manager kinda working
    works but is very slow and should be improved upon when we find a better
    way

---
+++ Assets/Scripts/ChildManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildManager : MonoBehaviour
+{
+    GameObject[] children;
+
+    public float radius;
+    private Vector3 rotOffset;
+
+    void Awake()
+    {
+        UpdateChildren();
+    }
+
+    public void UpdateChildren()
+    {
+        //Get all children
+        children = new GameObject[gameObject.transform.childCount];
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            children[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+
+        //Arrange children in circle
+
+        //METHOD 1 - TRIG (NOT VERY GOOD)
+        float rot = (2 * Mathf.PI) / children.Length;
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            float totalRot = (rot * i) + (Mathf.PI / 2);
+            Vector3 trigRot = new Vector3(Mathf.Cos(totalRot), Mathf.Sin(totalRot), 0);
+            
+            children[i].transform.position = trigRot * radius;
+        }
+
+        //METHOD 2 - FAKE TRANSFORM (ABOUT THE SAME)
+        /*rotOffset = new Vector3(0, 0, 360 / children.Length);
+        GameObject tempRot = new GameObject();  //Use this objects rotation for now because it seems the easiest
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            Vector3 totalRot = rotOffset * i;
+
+            tempRot.transform.position = transform.position;
+            tempRot.transform.eulerAngles = totalRot;
+
+            Ray childRay = new Ray(transform.position, tempRot.transform.up);   //If you want to improve this keep in mind that the ray's direction is normalized
+            children[i].transform.position = childRay.GetPoint(radius);
+        }
+        Destroy(tempRot);*/
+    }
+}
--- Assets/Scripts/Test_Script.cs
+++ Assets/Scripts/Test_Script.cs
@@ -3,19 +3,6 @@ using System.Collections;
 
 public class Test_Script : MonoBehaviour {
 
-    public GameObject[] children;
-    public GameObject ball;
-    // Use this for initialization
-    void Start() {
-        children = new GameObject[gameObject.transform.childCount];
-        for (int i = 0; i < gameObject.transform.childCount; i++)
-        {
-            children[i] = gameObject.transform.GetChild(i).gameObject;
-        }
-
-        put_script_into(ball, children[0]);
-
-    }
     void put_script_into(GameObject go, GameObject child_script)
     {
         child_script.transform.SetParent(go.transform, false);
