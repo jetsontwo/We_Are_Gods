0cf599ae1b140f1037133bcc9f8364cdaafa1f1a
jetsontwo <jetsontwo@gmail.com>
2016-10-18 19:10:27 -0700

    Created Test Script as Proof of Concept for Moving Child Scripts

--- Assets/Scripts/Test_Script.cs
+++ Assets/Scripts/Test_Script.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Test_Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject[] children;
+    public GameObject ball;
+    // Use this for initialization
+    void Start() {
+        children = new GameObject[gameObject.transform.childCount];
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            children[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+
+        put_script_into(ball, children[0]);
+
+    }
+    void put_script_into(GameObject go, GameObject child_script)
+    {
+        child_script.transform.SetParent(go.transform, false);
+    }
 }
