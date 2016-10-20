fc52ff7e6aa94de8d547e0a9469436bfa7fd653d
Roger <roger@dalkefamily.com>
2016-10-19 22:53:41 -0700

    Added game component folder and ChangeSolid component

---
+++ Assets/Scripts/Game Components/ChangeSolid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSolid : MonoBehaviour, Mechanic_Interface
+{
+    public void AddGameComponent()
+    {
+        GetComponentInParent<Collider2D>().enabled = false;
+    }
+
+    public void RemoveGameComponent()
+    {
+        GetComponentInParent<Collider2D>().enabled = true;
+    }
+}
