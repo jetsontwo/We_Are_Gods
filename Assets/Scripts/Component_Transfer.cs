8a68da5203a98a244e9095daef86d340eedd5273
jetsontwo <jetsontwo@gmail.com>
2016-10-24 21:25:09 -0700

    Updated Movement Animation and Component Transfer

--- Assets/Scripts/Component_Transfer.cs
+++ Assets/Scripts/Component_Transfer.cs
@@ -20,8 +20,15 @@ public class Component_Transfer : MonoBehaviour {
 
     void Transfer_Component(GameObject objectToTransferTo, GameObject componentToTransfer)
     {
+        Transform old_parent = componentToTransfer.transform.parent;
         //Sets the parent of the game component to the one specified
         componentToTransfer.transform.SetParent(objectToTransferTo.transform, false);
+
+        //Updates the children of the object that had the component removed and that of the new parent
+        old_parent.GetComponent<ChildManager>().UpdateChildren();
+        componentToTransfer.transform.parent.GetComponent<ChildManager>().UpdateChildren();
+        componentToTransfer.GetComponent<SpriteRenderer>().enabled = false;
+        componentToTransfer.GetComponent<BoxCollider2D>().enabled = false;
 
         //Runs the functions to reset the game component as its moved between GameObjects
         componentToTransfer.GetComponent<Mechanic_Interface>().RemoveGameComponent();
--- Assets/Scripts/Game Components/Player_Movement.cs
+++ Assets/Scripts/Game Components/Player_Movement.cs
@@ -7,6 +7,7 @@ public class Player_Movement : MonoBehaviour, Mechanic_Interface
     public float speed, max_vel;
     private float horiz_move;
     private Rigidbody2D rb;
+    private Animator ar;
 
     void Start()
     {
@@ -20,21 +21,42 @@ public class Player_Movement : MonoBehaviour, Mechanic_Interface
         if (Input.GetButton("Horizontal"))
         {
             horiz_move = Input.GetAxis("Horizontal");
+            if (horiz_move > 0)
+            {
+                ar.SetBool("walk_right", true);
+                ar.SetBool("walk_left", false);
+                ar.SetBool("Idle", false);
+            }
+            else
+            {
+                ar.SetBool("walk_left", true);
+                ar.SetBool("walk_right", false);
+                ar.SetBool("Idle", false);
+            }
             if (rb.velocity.magnitude < max_vel)
             {
                 rb.velocity += new Vector2(horiz_move * speed * Time.deltaTime, 0);
             }
+
         }
         else
         {
             rb.velocity -= new Vector2(rb.velocity.x, 0) * Time.deltaTime * 5;
+            ar.SetBool("walk_left", false);
+            ar.SetBool("walk_right", false);
+        }
+
+        if(rb.velocity.magnitude <= 0.2f)
+        {
+            ar.SetBool("Idle", true);
         }
     }
 
     public void AddGameComponent()
     {
         //Finds the new Rigidbody2D to act upon and sets the position of the game component on top of the new Player or actable object
-        rb = GetComponentInParent<Rigidbody2D>();
+        rb = transform.parent.GetComponent<Rigidbody2D>();
+        ar = transform.parent.GetComponent<Animator>();
         transform.localPosition = Vector3.zero;
     }
 
