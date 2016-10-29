2458168b4be5b2141fe81c4cdc79bd397fadbbaa
Roger <roger@dalkefamily.com>
2016-10-28 20:55:37 -0700

    Added editor script to automatically convert sprites to point filter

---
+++ Assets/Editor/SpriteImporter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SpriteImporter : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.filterMode = FilterMode.Point;
+    }
+}
