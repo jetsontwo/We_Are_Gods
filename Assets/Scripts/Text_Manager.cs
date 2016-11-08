0f1eda96c23c98dfa32f94e5cc829139c0b9605f
jetsontwo <jetsontwo@gmail.com>
2016-11-07 21:35:16 -0800

    Created Blip Sounds for Text and Particle Effects for Transferrable Objects :100: :100: :100:

--- Assets/Scripts/Text_Manager.cs
+++ Assets/Scripts/Text_Manager.cs
@@ -6,19 +6,22 @@ public class Text_Manager : MonoBehaviour {
 
     public string[] dialogue_array;
     public Text dialogue_box;
-    public float text_speed, time_between_text;
+    public float text_speed, time_between_text, beginning_wait;
     public GameObject text_background;
+    public AudioSource blip;
     private RawImage background_image;
 
 	
 	// Update is called once per frame
 	void Start () {
         background_image = text_background.GetComponent<RawImage>();
+        background_image.enabled = false;
         StartCoroutine(Show_Text(dialogue_array));
 	}
 
     IEnumerator Show_Text(string[] text)
     {
+        yield return new WaitForSeconds(beginning_wait);
         background_image.enabled = true;
         for(int i = 0; i < text.Length; i++)
         {
@@ -27,6 +30,8 @@ public class Text_Manager : MonoBehaviour {
             {
                 str += text[i][j];
                 dialogue_box.text = str;
+                blip.Play();
+                blip.pitch = Random.Range(0.5f, 0.6f);
                 yield return new WaitForSeconds(text_speed);
             }
             yield return new WaitForSeconds(time_between_text);
