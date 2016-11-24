ddbbc27cab9fd50554fe4130bcc9be9131ad898c
Roger <roger@dalkefamily.com>
2016-11-23 21:49:57 -0800

    Adjusted text manager and level 1
    Started to update text manager but i have no idea what andrew was
    planning to do with it.

--- Assets/Scripts/Text_Manager.cs
+++ Assets/Scripts/Text_Manager.cs
@@ -1,55 +1,66 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
 
 public class Text_Manager : MonoBehaviour {
 
-    public string[] dialogue_array;
+    public DialogueSegment[] dialogue_array;
     public Text dialogue_box;
     public float text_speed, time_between_text, beginning_wait;
+    public Font miltiadesFont, hrodebertFont;
     public GameObject text_background, exit_level, cur_touched_object, transferred_component;
     public AudioSource blip;
     private RawImage background_image;
     public string command;
-    private bool getToEndOfText;
+    private bool getToEndOfText = false;
 
-
-    // Update is called once per frame
+    
     void Awake()
     {
-        getToEndOfText = false;
         background_image = text_background.GetComponent<RawImage>();
         background_image.enabled = false;
+
         if (SceneManager.GetActiveScene().buildIndex == 1)
             StartCoroutine(First_Level(dialogue_array));
         else if (SceneManager.GetActiveScene().buildIndex == 2)
             StartCoroutine(Second_Level(dialogue_array));
     }
 
-    IEnumerator First_Level(string[] text)
+    IEnumerator First_Level(DialogueSegment[] text)
     {
         yield return new WaitForSeconds(beginning_wait);
         background_image.enabled = true;
 
         for(int i = 0; i < text.Length; i++)
         {
+            if(text[i].character == DialogueSegment.CharacterList.MILTIADES)
+            {
+                dialogue_box.font = miltiadesFont;
+            }
+            else if (text[i].character == DialogueSegment.CharacterList.HRODEBERT)
+            {
+                dialogue_box.font = hrodebertFont;
+            }
+
             string str = "";
-            for (int j = 0; j < text[i].Length; j++)
+            for (int j = 0; j < text[i].dialogueText.Length; j++)
             {
                 if(getToEndOfText)
                 {
-                    dialogue_box.text = text[i];
+                    dialogue_box.text = text[i].dialogueText;
                     getToEndOfText = false;
                     break;
                 }
-                str += text[i][j];
+                str += text[i].dialogueText[j];
                 dialogue_box.text = str;
+
+                blip.pitch = UnityEngine.Random.Range(1.1f, 1.15f);
                 blip.Play();
-                blip.pitch = Random.Range(1.1f, 1.15f);
+
                 yield return new WaitForSeconds(text_speed);
             }
-
             yield return new WaitForSeconds(time_between_text);
         }
 
@@ -69,42 +80,42 @@ public class Text_Manager : MonoBehaviour {
         return cur_touched_object.name.Equals("Tree") && transferred_component.name.Equals("Run_Right");
     }
 
-    IEnumerator Second_Level(string[] text)
+    IEnumerator Second_Level(DialogueSegment[] text)
     {
         yield return new WaitForSeconds(beginning_wait);
         background_image.enabled = true;
 
         string str = "";
-        for (int j = 0; j < text[0].Length; j++)
+        for (int j = 0; j < text[0].dialogueText.Length; j++)
         {
             if (getToEndOfText)
             {
-                dialogue_box.text = text[1];
+                dialogue_box.text = text[0].dialogueText;
                 getToEndOfText = false;
                 break;
             }
-            str += text[0][j];
+            str += text[0].dialogueText[j];
             dialogue_box.text = str;
             blip.Play();
-            blip.pitch = Random.Range(0.6f, 0.65f);
+            blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
             yield return new WaitForSeconds(text_speed);
         }
         yield return new WaitForSeconds(time_between_text);
         dialogue_box.text = "";
         yield return new WaitForSeconds(5);
         str = "";
-        for (int j = 0; j < text[1].Length; j++)
+        for (int j = 0; j < text[1].dialogueText.Length; j++)
         {
             if (getToEndOfText)
             {
-                dialogue_box.text = text[1];
+                dialogue_box.text = text[1].dialogueText;
                 getToEndOfText = false;
                 break;
             }
-            str += text[1][j];
+            str += text[1].dialogueText[j];
             dialogue_box.text = str;
             blip.Play();
-            blip.pitch = Random.Range(0.6f, 0.65f);
+            blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
             yield return new WaitForSeconds(text_speed);
         }
         dialogue_box.text = "";
@@ -113,51 +124,51 @@ public class Text_Manager : MonoBehaviour {
         if (cur_touched_object.name.Equals("TurtleCoon"))
         {
             str = "";
-            for (int j = 0; j < text[4].Length; j++)
+            for (int j = 0; j < text[4].dialogueText.Length; j++)
             {
                 if (getToEndOfText)
                 {
-                    dialogue_box.text = text[1];
+                    dialogue_box.text = text[1].dialogueText;
                     getToEndOfText = false;
                     break;
                 }
-                str += text[1][j];
+                str += text[1].dialogueText[j];
                 dialogue_box.text = str;
                 blip.Play();
-                blip.pitch = Random.Range(0.6f, 0.65f);
+                blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
                 yield return new WaitForSeconds(text_speed);
             }
             str = "";
-            for (int j = 0; j < text[5].Length; j++)
+            for (int j = 0; j < text[5].dialogueText.Length; j++)
             {
                 if (getToEndOfText)
                 {
-                    dialogue_box.text = text[1];
+                    dialogue_box.text = text[1].dialogueText;
                     getToEndOfText = false;
                     break;
                 }
-                str += text[1][j];
+                str += text[1].dialogueText[j];
                 dialogue_box.text = str;
                 blip.Play();
-                blip.pitch = Random.Range(0.6f, 0.65f);
+                blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
                 yield return new WaitForSeconds(text_speed);
             }
         }
         else if (cur_touched_object.name.Equals("Tree"))
         {
             str = "";
-            for (int j = 0; j < text[1].Length; j++)
+            for (int j = 0; j < text[1].dialogueText.Length; j++)
             {
                 if (getToEndOfText)
                 {
-                    dialogue_box.text = text[1];
+                    dialogue_box.text = text[1].dialogueText;
                     getToEndOfText = false;
                     break;
                 }
-                str += text[1][j];
+                str += text[1].dialogueText[j];
                 dialogue_box.text = str;
                 blip.Play();
-                blip.pitch = Random.Range(0.6f, 0.65f);
+                blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
                 yield return new WaitForSeconds(text_speed);
             }
         }
@@ -166,18 +177,18 @@ public class Text_Manager : MonoBehaviour {
         yield return new WaitUntil(level1_test_function2);
 
         str = "";
-        for (int j = 0; j < text[1].Length; j++)
+        for (int j = 0; j < text[1].dialogueText.Length; j++)
         {
             if (getToEndOfText)
             {
-                dialogue_box.text = text[1];
+                dialogue_box.text = text[1].dialogueText;
                 getToEndOfText = false;
                 break;
             }
-            str += text[1][j];
+            str += text[1].dialogueText[j];
             dialogue_box.text = str;
             blip.Play();
-            blip.pitch = Random.Range(0.6f, 0.65f);
+            blip.pitch = UnityEngine.Random.Range(0.6f, 0.65f);
             yield return new WaitForSeconds(text_speed);
         }
         yield return new WaitForSeconds(time_between_text);
@@ -191,10 +202,20 @@ public class Text_Manager : MonoBehaviour {
 
     void Update()
     {
-        getToEndOfText = false;
+        //getToEndOfText = false;
         if(Input.GetKeyDown(KeyCode.Return))
         {
             getToEndOfText = true;
         }
     }
+}
+
+[Serializable]
+public class DialogueSegment
+{
+    public enum CharacterList { HRODEBERT, MILTIADES }
+    public CharacterList character;
+
+    //public bool requireAction;    //Maybe add later
+    public string dialogueText;
 }
